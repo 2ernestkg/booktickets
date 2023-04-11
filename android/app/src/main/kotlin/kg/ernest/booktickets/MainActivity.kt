850d5a9f905270c823437c918a0e1bcdb194ffd5
Ernest Kuttubaev <2ernest.kg@gmail.com>
2023-04-11 06:34:54 +0600

    Bottom Navigation Bar

---
+++ android/app/src/main/kotlin/kg/ernest/booktickets/MainActivity.kt
@@ -0,0 +1,6 @@
+package kg.ernest.booktickets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/screens/bottom_bar.dart
@@ -0,0 +1,66 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedItemIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    const Text('Home'),
+    const Text('Search'),
+    const Text('Tickets'),
+    const Text('Profile'),
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedItemIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Tickets'),
+      ),
+      body: Center(
+        child: _widgetOptions[_selectedItemIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedItemIndex,
+        onTap: _onItemTap,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xff526480),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: 'Tickets'),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
